49908d4d933f6b8ab50d072d2c68b64b45a5e155
Keshav <keshavgogia57@gmail.com>
2023-12-29 16:10:30 +0530

    modified port

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import express from "express";
 
 
 const app = express();
-const port = 8000;
 
 app.use(express.static("public"));
 app.get("/",(req,res)=>{
@@ -24,6 +23,6 @@ app.get("/skills",(req,res) => {
 app.get("/projects",(req,res)=>{
   res.render("projects.ejs");
 })
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Server running on port ${port}`);
+// });
